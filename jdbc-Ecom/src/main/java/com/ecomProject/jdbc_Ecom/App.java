66ddd19593633be7b46b44222ebd255e60783a4f
Jyoti Pradhan <jyotikumaripradhan000@gmail.com>
2026-07-15 13:39:06 +0530

    Project initialized

---
+++ jdbc-Ecom/src/main/java/com/ecomProject/jdbc_Ecom/App.java
@@ -0,0 +1,13 @@
+package com.ecomProject.jdbc_Ecom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
