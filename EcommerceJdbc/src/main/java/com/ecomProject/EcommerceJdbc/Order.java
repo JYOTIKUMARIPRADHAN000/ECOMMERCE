6d57aff164282c345d528f415e8e8050c03849c0
ramji4532 <117074819+ramji4532@users.noreply.github.com>
2026-07-16 14:40:24 +0530

    Revert "order service added"

--- EcommerceJdbc/src/main/java/com/ecomProject/EcommerceJdbc/Order.java
+++ EcommerceJdbc/src/main/java/com/ecomProject/EcommerceJdbc/Order.java
@@ -1,5 +1,10 @@
 package com.ecomProject.EcommerceJdbc;
 
 public class Order {
+	public static void main(String args[])
+	{
+		System.out.println("order service ");
+		System.out.println("order service 2 ");
+	}
 
 }
